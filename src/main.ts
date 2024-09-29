a7f93701673d8088329ed63662bc846f2da86a7d
Prooksius <prooksius@gmail.com>
2024-09-29 16:27:42 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
   );
   await app.listen();
   // const app = await NestFactory.create(AppModule);
-  // await app.listen(3000);
+  // await app.listen(3000); et4tgwnkejfew
 }
 bootstrap();
